612e9782eb17d9fc0659116a6c0a32725fa23b64
muammar johnson <muammarj@gmail.com>
2021-02-08 19:04:49 -0600

    added binding

--- app/src/main/java/com/example/countryselector/view/MainActivity.java
+++ app/src/main/java/com/example/countryselector/view/MainActivity.java
@@ -4,16 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
 
 import com.example.countryselector.R;
+import com.example.countryselector.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     LinearLayoutManager linearLayoutManager;
-
+    ActivityMainBinding activityMainBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
 
-        setContentView(R.layout.activity_main);
+        setContentView(activityMainBinding.getRoot());
     }
 }
